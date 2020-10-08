08a65b83beabd3deb643a22708ebe05cf371ac31
Drew Bradley <williamandrewb@gmail.com>
2020-10-08 12:04:37 -0600

    Update function.js

--- functions.js
+++ functions.js
@@ -1,9 +1,27 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function sayHello(name){
+    if (name === "Will"){
+        return `No more testing ${name}!`
+    }else{
+        return `Hi there ${name}!`
+    }
+}
+
+function buildCar(color, type){
+    if (color || type){
+        return {color: color,
+                type: type} 
+    }else{
+        return {}
+        }
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
